2803462a371fa473c1097577942f0ff3734a08ee
Sidney de Geus <sidneydegeus@hotmail.com>
2019-05-09 06:56:36 +0200

    transfer code to component

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
@@ -26,6 +27,6 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 };
